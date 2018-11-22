71057dde7d47bacfeaf260dad66b64c7f2c1c011
Remco Ros <remcoros@live.nl>
2018-11-22 19:38:33 +0100

    Default ASP Core API Project with a test

---
+++ src/Tests/ValuesControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using MvcTestingReSharper;
+using Xunit;
+
+namespace Tests
+{
+    public class ValuesControllerTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _webapp;
+
+        public ValuesControllerTests(WebApplicationFactory<Startup> webapp)
+        {
+            _webapp = webapp;
+        }
+
+        [Fact]
+        public async Task CanGetValues()
+        {
+            var client = _webapp.CreateClient();
+            var response = await client.GetAsync("/api/values/1");
+            
+            response.EnsureSuccessStatusCode();
+
+            var value = await response.Content.ReadAsStringAsync();
+            
+            Assert.Equal("value", value);
+        }
+    }
+}
